80c42e651c2eeffbae6b29767aaa4181d5379171
intraceting <traceting@gmail.com>
2025-09-12 23:25:59 +0800

    update

--- tools/get-compiler-platform.sh
+++ tools/get-compiler-platform.sh
@@ -22,12 +22,40 @@ checkReturnCode()
 TARGET_MACHINE=$(${SHELLDIR}/get-compiler-machine.sh "${1}")
 checkReturnCode
 
-#提取第一段当平台。
-TARGET_PLATFORM=$(echo ${TARGET_MACHINE} | cut -d - -f 1)
-
 #
-if [ "${TARGET_PLATFORM}" == "" ];then
+if [ "${TARGET_MACHINE}" == "" ];then
 exit 127
 fi
 
-echo "${TARGET_PLATFORM}"
+#拆分到数组中.
+IFS="-" read -ra TARGET_TRIPLE_VECTOR <<< "${TARGET_MACHINE}"
+
+if [ "${TARGET_TRIPLE_VECTOR[0]}" == "x86_64" ] || [ "${TARGET_TRIPLE_VECTOR[0]}" == "aarch64" ];then
+{
+    echo "${TARGET_TRIPLE_VECTOR[0]}"
+}
+elif [ "${TARGET_TRIPLE_VECTOR[0]}" == "armv8" ] || [ "${TARGET_TRIPLE_VECTOR[0]}" == "armv7" ] || [ "${TARGET_TRIPLE_VECTOR[0]}" == "armv6" ];then
+{
+    if [[ ${TARGET_TRIPLE_VECTOR[-1]} == *eabihf ]]; then
+        echo "${TARGET_TRIPLE_VECTOR[0]}hl"
+    elif [[ ${TARGET_TRIPLE_VECTOR[-1]} == *eabi ]]; then
+        echo "${TARGET_TRIPLE_VECTOR[0]}l"
+    else 
+        echo "${TARGET_TRIPLE_VECTOR[0]}"
+    fi
+}
+elif [ "${TARGET_TRIPLE_VECTOR[0]}" == "arm" ];then
+{
+    if [[ ${TARGET_TRIPLE_VECTOR[-1]} == *eabihf ]]; then
+        echo "armv7hl"
+    elif [[ ${TARGET_TRIPLE_VECTOR[-1]} == *eabi ]]; then
+        echo "armv7l"
+    else 
+        echo "armv7"
+    fi
+}
+else 
+{
+    echo "${TARGET_TRIPLE_VECTOR[0]}"
+}
+fi
