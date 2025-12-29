8eb0be05c7effa099bd8ad70286bb289a92e8c14
intraceting <traceting@gmail.com>
2025-12-29 11:17:52 +0800

    update

--- tools/get-compiler-multiarch.sh
+++ tools/get-compiler-multiarch.sh
@@ -25,5 +25,32 @@ COMPILER_BIN=$(which "${1}")
 TARGET_MULTIARCH=$(${COMPILER_BIN} "-print-multiarch" 2>>/dev/null)
 checkReturnCode
 
+#不为空直接返回.
+if [ "${TARGET_MULTIARCH}" != "" ];then
+	echo "${TARGET_MULTIARCH}"
+	exit 0
+fi
+
 #
-echo "${TARGET_MULTIARCH}"
+TARGET_MACHINE=$(${COMPILER_BIN} "-dumpmachine" 2>>/dev/null)
+checkReturnCode
+
+#为空直接返回.
+if [ "${TARGET_MACHINE}" == "" ];then
+	echo "${TARGET_MACHINE}"
+	exit 0
+fi
+
+#拆分到数组.
+IFS='-' read -r -a ARR <<< "$TARGET_MACHINE"
+LEN=${#ARR[@]}
+
+#少于三段直接返回, 大于三段则使用0,LEN-2,LEN-1拼接起来.
+if [ ${LEN} -lt 3 ];then
+	echo "${TARGET_MACHINE}"
+else
+	echo "${ARR[0]}-${ARR[LEN-2]}-${ARR[LEN-1]}"
+fi
+
+
+
