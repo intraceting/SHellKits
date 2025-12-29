dfa80999dcadba6e3ab43d8eba67bc33f457da52
intraceting <traceting@gmail.com>
2025-12-29 09:34:48 +0800

    update

--- tools/copy-compiler-needed.sh
+++ tools/copy-compiler-needed.sh
@@ -22,23 +22,23 @@ MACHINE=$(${SHELLDIR}/get-compiler-machine.sh ${COMPILER_BIN})
 
 if [ -f ${SYSROOT}/lib64/libgcc_s.so.1 ];then
     mkdir -p ${TARGET_PATH}
-    cp -fP ${SYSROOT}/lib64/libgcc_s.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib64/libgfortran.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib64/libgomp.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib64/libstdc++.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib64/libatomic.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib64/libgcc_s*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib64/libgfortran*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib64/libgomp*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib64/libstdc++*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib64/libatomic*.so.* ${TARGET_PATH}/
 elif [ -f ${SYSROOT}/lib/libgcc_s.so.1 ];then
     mkdir -p ${TARGET_PATH}
-    cp -fP ${SYSROOT}/lib/libgcc_s.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib/libgfortran.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib/libgomp.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib/libstdc++.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib/libatomic.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib/libgcc_s*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib/libgfortran*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib/libgomp*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib/libstdc++*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib/libatomic*.so.* ${TARGET_PATH}/
 elif [ -f ${SYSROOT}/lib/${MACHINE}/libgcc_s.so.1 ];then
     mkdir -p ${TARGET_PATH}
-    cp -fP ${SYSROOT}/lib/${MACHINE}/libgcc_s.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib/${MACHINE}/libgfortran.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib/${MACHINE}/libgomp.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib/${MACHINE}/libstdc++.so.* ${TARGET_PATH}/
-    cp -fP ${SYSROOT}/lib/${MACHINE}/libatomic.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib/${MACHINE}/libgcc_s*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib/${MACHINE}/libgfortran*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib/${MACHINE}/libgomp*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib/${MACHINE}/libstdc++*.so.* ${TARGET_PATH}/
+    cp -fP ${SYSROOT}/lib/${MACHINE}/libatomic*.so.* ${TARGET_PATH}/
 fi
