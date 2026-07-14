fbb423a14b39c4e0d6ff38acd4c440c3db213874
intraceting <traceting@gmail.com>
2026-07-14 23:05:25 +0800

    update

--- tools/test-c-std/sample.c
+++ tools/test-c-std/sample.c
@@ -3,7 +3,7 @@
 // This file is part of SHELLKITS.
 //
 // Copyright (c) 2025 The SHELLKITS project authors. All Rights Reserved.
-// Copyright (c) 2021 The ABCDK project authors. All Rights Reserved.
+// 
 //
 //
 #include <stdio.h>
