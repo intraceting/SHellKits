0cdca27ddbb3ec089c7e6c4aa9380d2905d9d89b
intraceting <traceting@gmail.com>
2026-07-21 22:08:20 +0800

    update

--- makefile.install.mk
+++ makefile.install.mk
@@ -13,7 +13,8 @@ install-handy-utils:
 	cp -rfP $(MAKEFILE_DIR)/handy-utils/. ${INSTALL_PREFIX}/shellkits/handy-utils/
 	find ${INSTALL_PREFIX}/shellkits/handy-utils -type d -exec chmod 0755 {} \;
 	find ${INSTALL_PREFIX}/shellkits/handy-utils -type f -exec chmod 0644 {} \;
-	find ${INSTALL_PREFIX}/shellkits/handy-utils -type f -name *.sh -exec chmod 0555 {} \;
+#双引号很重要, 下同. 如果未加双引号, '*.sh' 会被makefile解析并匹配成 '.sh' .
+	find ${INSTALL_PREFIX}/shellkits/handy-utils -type f -name "*.sh" -exec chmod 0555 {} \;
 
 #
 uninstall-handy-utils:
@@ -28,7 +29,7 @@ install-cross-toolchain:
 	cp -rfP $(MAKEFILE_DIR)/cross-toolchain/. ${INSTALL_PREFIX}/shellkits/cross-toolchain/
 	find ${INSTALL_PREFIX}/shellkits/cross-toolchain -type d -exec chmod 0755 {} \;
 	find ${INSTALL_PREFIX}/shellkits/cross-toolchain -type f -exec chmod 0644 {} \;
-	find ${INSTALL_PREFIX}/shellkits/cross-toolchain -type f -name *.sh -exec chmod 0555 {} \;
+	find ${INSTALL_PREFIX}/shellkits/cross-toolchain -type f -name "*.sh" -exec chmod 0555 {} \;
 
 #
 uninstall-cross-toolchain:
@@ -43,7 +44,7 @@ install-fast-c-cxx:
 	cp -rfP $(MAKEFILE_DIR)/fast-c-cxx/. ${INSTALL_PREFIX}/shellkits/fast-c-cxx/
 	find ${INSTALL_PREFIX}/shellkits/fast-c-cxx -type d -exec chmod 0755 {} \;
 	find ${INSTALL_PREFIX}/shellkits/fast-c-cxx -type f -exec chmod 0644 {} \;
-	find ${INSTALL_PREFIX}/shellkits/fast-c-cxx -type f -name *.sh -exec chmod 0555 {} \;
+	find ${INSTALL_PREFIX}/shellkits/fast-c-cxx -type f -name "*.sh" -exec chmod 0555 {} \;
 
 #
 uninstall-fast-c-cxx:
@@ -59,7 +60,7 @@ install-no-trouble-lfs:
 	cp -rfP $(MAKEFILE_DIR)/no-trouble-lfs/. ${INSTALL_PREFIX}/shellkits/no-trouble-lfs/
 	find ${INSTALL_PREFIX}/shellkits/no-trouble-lfs -type d -exec chmod 0755 {} \;
 	find ${INSTALL_PREFIX}/shellkits/no-trouble-lfs -type f -exec chmod 0644 {} \;
-	find ${INSTALL_PREFIX}/shellkits/no-trouble-lfs -type f -name *.sh -exec chmod 0555 {} \;
+	find ${INSTALL_PREFIX}/shellkits/no-trouble-lfs -type f -name "*.sh" -exec chmod 0555 {} \;
 
 #
 uninstall-no-trouble-lfs:
--- no-trouble-lfs/locate.sh
+++ no-trouble-lfs/locate.sh
@@ -34,9 +34,7 @@ TMP_HOME_B=$(realpath -s "${PWD}")
 
 #不允许脚本所在目录运行.
 if [ "${TMP_HOME_A}" == "${TMP_HOME_B}" ];then
-{
     exit_if_error 1 "Scripts cannot be run from the directory where they are located." 1
-}
 fi
 
 #
